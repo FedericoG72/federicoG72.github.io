159bf031cbb55b70fa644c45976c996f5e4f23ee
zanzarabeo33 <71026680+zanzarabeo33@users.noreply.github.com>
2020-12-07 12:07:06 +0100

    Add files via upload

--- js/80ks_script.js
+++ js/80ks_script.js
@@ -1,15 +1,3 @@
-  var counter = 1;
-setInterval(function(){
-    document.getElementById('radio' + counter).checked = true;
-    counter++;
-    if(counter > 4){
-        counter = 1;
-        
-    }
-    
-    
-}, 5000);
-
 
 
 
@@ -32,3 +20,4 @@ $(window).scroll(function() {
     $('.logo-bar').hide();
   }
 });
+
