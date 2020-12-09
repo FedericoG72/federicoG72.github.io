fbab9dbb75a08fa917afa6c0c16de48ccdc3cfec
zanzarabeo33 <71026680+zanzarabeo33@users.noreply.github.com>
2020-12-09 09:47:20 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,3 +1,16 @@
+let deferredPrompt;
+
+window.addEventListener('beforeinstallprompt', (e) => {
+  // Prevent the mini-infobar from appearing on mobile
+  e.preventDefault();
+  // Stash the event so it can be triggered later.
+  deferredPrompt = e;
+  // Update UI notify the user they can install the PWA
+  showInstallPromotion();
+});
+
+
+
 const staticCacheName = 'site-static-v2';
 const dynamicCacheName = 'site-dynamic-v1';
 const assets = [
