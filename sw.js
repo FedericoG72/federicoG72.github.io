dee677a7898bd5a7c9b78a48f6102d40f25bcf8b
zanzarabeo33 <71026680+zanzarabeo33@users.noreply.github.com>
2020-12-09 10:41:15 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,96 +1,14 @@
-let deferredPrompt;
-
-window.addEventListener('beforeinstallprompt', (e) => {
-  // Prevent the mini-infobar from appearing on mobile
-  e.preventDefault();
-  // Stash the event so it can be triggered later.
-  deferredPrompt = e;
-  // Update UI notify the user they can install the PWA
-  showInstallPromotion();
-});
-
-
-
-const staticCacheName = 'site-static-v2';
-const dynamicCacheName = 'site-dynamic-v1';
-const assets = [
-  '/',
-  '/index.html',
-  '/js/app.js',
-  '/css/80ks_style.css',
-  '/img/dish.png',
-  'https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js',
-  'https://fonts.gstatic.com',
-  'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,400;0,500;0,700;0,900;1,300;1,400;1,500;1,700;1,900&display=swap',
-  '/fallback.html',
-  '/js/80ks_script.js',
-  '/dance32.html',
-  '/festival_review.html',
-  '/i_nostri_spettacoli.html',
-  '/live_is_life.html',
-  '/music_hall.html',
-  '/storia.html',
-  '/images/Logo80KS_R.png',
-  '/images/80ks_icona.png',
-  '/images/Festival.jpg',
-  '/images/Live_is_life.jpg',
-  '/images/Dance_32.jpg',
-  '/images/Music_Hall.jpg',
-];
-
-// cache size limit function
-const limitCacheSize = (name, size) => {
-  caches.open(name).then(cache => {
-    cache.keys().then(keys => {
-      if(keys.length > size){
-        cache.delete(keys[0]).then(limitCacheSize(name, size));
-      }
-    });
-  });
-};
-
 // install event
 self.addEventListener('install', evt => {
-  //console.log('service worker installed');
-  evt.waitUntil(
-    caches.open(staticCacheName).then((cache) => {
-      console.log('caching shell assets');
-      cache.addAll(assets);
-    })
-  );
+  console.log('service worker installed');
 });
 
 // activate event
 self.addEventListener('activate', evt => {
-  //console.log('service worker activated');
-  evt.waitUntil(
-    caches.keys().then(keys => {
-      //console.log(keys);
-      return Promise.all(keys
-        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
-        .map(key => caches.delete(key))
-      );
-    })
-  );
+  console.log('service worker activated');
 });
 
 // fetch event
 self.addEventListener('fetch', evt => {
-  //console.log('fetch event', evt);
-  evt.respondWith(
-    caches.match(evt.request).then(cacheRes => {
-      return cacheRes || fetch(evt.request).then(fetchRes => {
-        return caches.open(dynamicCacheName).then(cache => {
-          cache.put(evt.request.url, fetchRes.clone());
-          // check cached items size
-          limitCacheSize(dynamicCacheName, 15);
-          return fetchRes;
-        })
-      });
-    }).catch(() => {
-      if(evt.request.url.indexOf('.html') > -1){
-        return caches.match('/pages/fallback.html');
-      } 
-    })
-  );
+  console.log('fetch event', evt);
 });
