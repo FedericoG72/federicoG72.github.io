f2e43ed344b0750a8b20c531789d5d87ce8bca18
zanzarabeo33 <71026680+zanzarabeo33@users.noreply.github.com>
2020-12-23 15:24:23 +0100

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -3,3 +3,22 @@ if('serviceWorker' in navigator){
     .then((reg) => console.log('Service worker registrato', reg))
     .catch((err) => console.log('Service worker non registrato', err));
 }
+
+function richiediNotifica(){
+    Notification.requestPermission(function(result){
+        console.log('Scelta utente', result);
+        if (result !== 'permesso'){
+            console.log('Notifiche non permesse');
+        } else {
+            
+        }
+    })
+}
+
+var notificationButton = document.querySelectorAll(".enable-notification");
+if ('Notification' in window){
+    for (var i = 0; i < notificationButton.length; i++){
+        notificationButton[i].style.display = 'inline-block';
+        notificationButton[i].addEventListener('click', richiediNotifica);
+    }
+}
